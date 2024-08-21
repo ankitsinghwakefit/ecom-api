d14a503df953a9989ce46c85fff4ec3e02649a21
ankitsinghwakefit <ankitsinghwakefit@gmail.com>
2024-08-21 09:52:34 +0530

    lombok added to add boilerplate code

--- src/main/java/com/example/ecom_app/model/Category.java
+++ src/main/java/com/example/ecom_app/model/Category.java
@@ -4,36 +4,42 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
+@Entity(name = "categories") // name our table name
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long categoryId;
     private String categoryName;
 
-    public Long getCategoryId() {
-        return categoryId;
-    }
+    // public Long getCategoryId() {
+    // return categoryId;
+    // }
 
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
+    // public void setCategoryId(Long categoryId) {
+    // this.categoryId = categoryId;
+    // }
 
-    public String getCategoryName() {
-        return categoryName;
-    }
+    // public String getCategoryName() {
+    // return categoryName;
+    // }
 
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
+    // public void setCategoryName(String categoryName) {
+    // this.categoryName = categoryName;
+    // }
 
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
+    // public Category(Long categoryId, String categoryName) {
+    // this.categoryId = categoryId;
+    // this.categoryName = categoryName;
+    // }
 
-    public Category() {
-    }
+    // public Category() {
+    // }
 
 }
